edeb43a4b7017489467cd5fc7a54614659db252d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-06 20:15:25 +0100

    Jogo de Adivinhaçao

---
+++ Alura-C/jogoAdvinhacao/adivinhacao.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//imprimir o cabeçalho do nosso jogo
+int main(){
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+//variáveis
+    int numeroSecreto = 42;
+    int chute;
+    
+
+
+    printf("Qual é o seu chute? ");
+    scanf("%d",& chute);
+    printf("Seu chute foi %d * \n", chute);
+int acertou = (chute == numeroSecreto);
+    if (acertou){
+     printf("Parabéns você acertou! \n*");
+     printf("Jogue de novo para testar se sua sorte continua com você *");
+    }else{
+        if(chute > numeroSecreto){
+            printf("Sue chute foi maior que o numero secreto! \n");
+            
+        }if(chute < numeroSecreto){
+            printf("Seu chute foi menor que o numero secreto! \n");
+        }
+        
+        
+    };
+    
+
+    
+
+
+}
