5b38bf6efb43fcbed28906b64b9cba68eefd9c0d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-06 21:54:23 +0100

    Jogo da Adivinhaçao

--- Alura-C/jogoAdvinhacao/adivinhacao.c
+++ Alura-C/jogoAdvinhacao/adivinhacao.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
-//imprimir o cabeçalho do nosso jogo
-int main(){
+// imprimir o cabeçalho do nosso jogo
+int main()
+{
     printf("******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
 
-//variáveis
+    // variáveis
     int numeroSecreto = 48;
     int chute;
     
+    
 
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("Tentativa %d ",i);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d * \n", chute);
 
-    printf("Qual é o seu chute? ");
-    scanf("%d",& chute);
-    printf("Seu chute foi %d * \n", chute);
-
-int acertou = (chute == numeroSecreto);
-printf("teste chute %d",acertou);
-
-    if (acertou){
-     printf("Parabéns você acertou! \n*");
-     printf("Jogue de novo para testar se sua sorte continua com você *");
-    }else{
-        if(chute > numeroSecreto){
-            printf("Sue chute foi maior que o numero secreto! \n");
-            
-        }if(chute < numeroSecreto){
-            printf("Seu chute foi menor que o numero secreto! \n");
+    int acertou = (chute == numeroSecreto);
+        if (acertou)
+        {
+            printf("Parabéns você acertou! \n*");
+            printf("Jogue de novo para testar se sua sorte continua com você \n*");
+            break;
         }
-        
-        
-    };
-    
-
-    
-
-
+        else
+        {
+            int maior = chute > numeroSecreto;
+            if (maior)
+            {
+                printf("Sue chute foi maior que o numero secreto! \n");
+            }
+            else
+            {
+                printf("Seu chute foi menor que o numero secreto! \n");
+            }
+        };
+        /* code */
+    }
+    printf("******Fim de jogo******\n");
 }
