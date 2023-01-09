d3072a2a05eaa03b070b3784aff79e9d7363207b
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-09 20:27:45 +0100

    Listagen de atalhos

---
+++ Estudonauta-C/001-inprimindo-na-tela/index2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    printf("\n\tListagem\n");
+    printf("=============================");
+    printf("\n\\a\t=\tBeep\n");
+    printf("\\n\t=\tNova Linha\n");
+    printf("\\t\t=\tTabulação\n");
+    printf("\\\\\t=\tBarra\n");
+    printf("%%%%\t=\tPorcentagem\n");
+    printf("\\?\t=\tInterrogação\n");
+    printf("\\\"\"\t=\tAspas\n");
+    printf("=============================\n\n");
+}
