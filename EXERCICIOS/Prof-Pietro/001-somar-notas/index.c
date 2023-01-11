aad7aba305580e63bb183f9b9c500b2bf06a3191
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-11 21:46:18 +0100

    001 Somas das notas

---
+++ EXERCICIOS/Prof-Pietro/001-somar-notas/index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+float nota1, nota2, nota3, nota4;
+    
+printf("Digite a 1° nota\n");
+scanf("%f", &nota1);
+
+printf("Digite a 2° nota\n");
+scanf("%f", &nota2);
+
+printf("Digite a 3° nota\n");
+scanf("%f", &nota3);
+printf("Digite a 4° nota\n");
+scanf("%f", &nota4);
+
+float somaNota = nota1 + nota2 + nota3 + nota4 ;
+
+printf("A soma das notas é: %.2f\n",somaNota );
+}
