a3322761514360da6feab3136147c49b16a5961d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-14 00:03:25 +0100

    salvando string cadeia de caracteres

--- EXERCICIOS/Estudonauta/testes/index.c
+++ EXERCICIOS/Estudonauta/testes/index.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(){
-    setlocale(LC_ALL,"Portuguese");
-    char s[10];
-    scanf("%s",s);
-    fflush(stdin);printf("%s\n",s);
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    printf("Digite seu nome\n");
+    char s[30];
+    // scanf("%s",s);
+    // fflush(stdin);
 
-    scanf("%10[^\n]s", s);
+    // printf("Resultado scanf normal %s\n\n",s);
+
+    printf("Digite algo\n");
+    scanf("%30[^\n]s", s);
+    fflush(stdin);
+    printf("Resultado scanf APRIMORADO\n %s\n", s);
 }
