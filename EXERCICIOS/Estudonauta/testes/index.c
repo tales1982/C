1f94369b6cce2930263439164a7a0745cc59e105
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-14 07:56:32 +0100

    super scanf  ler string com spaço

--- EXERCICIOS/Estudonauta/testes/index.c
+++ EXERCICIOS/Estudonauta/testes/index.c
@@ -12,7 +12,7 @@ int main()
     // printf("Resultado scanf normal %s\n\n",s);
 
     printf("Digite algo\n");
-    scanf("%30[^\n]s", s);
+    scanf("%30[^\n]s", s);//scanf para ler string com espaços e nao da quebra de linha.
     fflush(stdin);
     printf("Resultado scanf APRIMORADO\n %s\n", s);
 }
