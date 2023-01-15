08d10bec36ba81bd945fb0e2eba2f4d003db4249
Tales Lima <tales_lima_1982@hotmail.com>
2023-01-15 15:55:53 +0100

    rodando

--- EXERCICIOS/Estudonauta/005/index.c
+++ EXERCICIOS/Estudonauta/005/index.c
@@ -11,19 +11,21 @@ int main()
     printf("------------------------------\n");
     fflush(stdin);
     printf("NOME: ");
-    fgets(nome1, 30, stdin);
+    scanf("%30[^\n]s", nome1);
+    // fgets(nome1, 30, stdin);
     fflush(stdin);
     printf("SEXO [M/F] : ");
     sexo1 = getchar();
     fflush(stdin);
     printf("NOTA: ");
-    scanf("%f",&nota1);
+    scanf("%f", &nota1);
     fflush(stdin);
     printf("Cadastrando a segunda pessoa:\n");
     printf("------------------------------\n");
     fflush(stdin);
     printf("NOME: ");
-    fgets(nome2, 30, stdin);
+    scanf("%30[^\n]s", nome2);
+    // fgets(nome2, 30, stdin);
     fflush(stdin);
     printf("SEXO [M/F]: ");
     sexo2 = getchar();
@@ -35,7 +37,8 @@ int main()
     printf("------------------------------\n");
     fflush(stdin);
     printf("NOME: ");
-    fgets(nome3, 30, stdin);
+    scanf("%30[^\n]s", nome3);
+    // fgets(nome3, 30, stdin);
     fflush(stdin);
     printf("SEXO [M/F]: ");
     sexo3 = getchar();
@@ -45,9 +48,8 @@ int main()
     fflush(stdin);
     printf("LISTAGEM COMPLETA");
     printf("------------------------------\n");
-    printf("Nome:%sSexo:%c\nNota:%.2f\n", nome1, sexo1, nota1);
-    printf("------------------------------\n");
-    printf("Nome:%sSexo:%c\nNota:%.2f\n", nome2, sexo2,nota2);
-    printf("------------------------------\n");
-    printf("Nome:%sSexo:%c\nNota:%.2f\n", nome3, sexo3, nota3);
+    printf("NOME\t\tSEXO\tNOTA\n");
+    printf("%s\t\t%c\t%.2f\n", nome1, sexo1, nota1);
+    printf("%s\t\t%c\t%.2f\n", nome2, sexo2, nota2);
+    printf("%s\t\t%c\t%.2f\n", nome3, sexo3, nota3);
 }
