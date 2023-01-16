08d94ea4b163604853835b57370b2a6be18075ac
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-16 23:02:16 +0100

    Antecessor e Sucessor

--- EXERCICIOS/Estudonauta/007/index.c
+++ EXERCICIOS/Estudonauta/007/index.c
@@ -9,11 +9,11 @@ int main(){
     printf("Digite um numero: \n");
     scanf("%i",&n);
     fflush(stdin);
-    printf("---<<< Analizando o numero (%d) >>>---\n",n);
+    printf("---<<< Analizando o numero (%i) >>>---\n",n);
     n--;
-    printf("O anteçcessor é %d \n",n);
+    printf("O anteçcessor é %i \n",n);
     n++;
-    printf("O sucessor é %d \n",n + 1 );
+    printf("O sucessor é %i \n",n + 1 );
 
 
 }
