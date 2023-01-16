e439d0d53888ab611f349a9d5b4ee2a5edde7b53
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-16 20:04:17 +0100

    CÁLCULO DE REAJUSTE SALARIAL

--- EXERCICIOS/Prof-Pietro/004-Almento-d-salario/index.c
+++ EXERCICIOS/Prof-Pietro/004-Almento-d-salario/index.c
@@ -4,13 +4,22 @@
 int main(){
     setlocale(LC_ALL,"Portuguese");
 
-    float salario, reajuste,salarioAjustado;
+    float salario, reajuste,salarioFinal;
+    
     fflush(stdin);
+    printf("CALCULO DE REAJUSTE SALARIAL\n");
+    printf("------------------------------------\n");
     printf("Digite o seu salario?\n");
     scanf("%f",&salario);
     fflush(stdin);
-    printf("Digite o valor do ajuste?:\n");
+    printf("Digite a porcetagem do ajuste?:\n");
     scanf("%f",&reajuste);
     fflush(stdin);
+    printf("--------<<<<<< CALCULANDO >>>>>>--------\n");
+    printf("Salario atual: %.2f\n",salario);
+    printf("Porcentagem do aumento: %.2f %%\n",reajuste);
+    printf("Valor do reajuste: %.2f\n",(reajuste/100)*salario);
+    salarioFinal = (reajuste/100)*salario + salario;
+    printf("Salario reajustado: %.2f\n",salarioFinal);
     //
 }
