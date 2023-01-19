14d045f40ec2c6959785d4a7fb33fe6a8ccb60d5
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-19 21:06:08 +0100

    Cadastro de pessoas

--- EXERCICIOS/Estudonauta/005/index.c
+++ EXERCICIOS/Estudonauta/005/index.c
@@ -7,76 +7,52 @@ int main()
     char nome1[30], nome2[30], nome3[30];
     char sexo1, sexo2, sexo3;
     float nota1, nota2, nota3;
-
     printf("Cadastrando a primeira pessoa:\n");
     printf("------------------------------\n");
-    
     printf("NOME: ");
     scanf("%30[^\n]s", nome1);
-int    ch;
-  char   buf[BUFSIZ];
-  while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    // fgets(nome1, 30, stdin);
+    clearBuffer();
     printf("SEXO [M/F] : ");
     sexo1 = getchar();
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
-  
+    clearBuffer();
     printf("NOTA: ");
     scanf("%f", &nota1);
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    clearBuffer();
     printf("Cadastrando a segunda pessoa:\n");
     printf("------------------------------\n");
     printf("NOME: ");
     scanf("%30[^\n]s", nome2);
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    // fgets(nome2, 30, stdin);
+    clearBuffer();
     printf("SEXO [M/F]: ");
     sexo2 = getchar();
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    clearBuffer();
     printf("NOTA: ");
     scanf("%f", &nota2);
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    clearBuffer();
     printf("Cadastrando a terceira pessoa:\n");
-    printf("------------------------------\n");   
+    printf("------------------------------\n");
     printf("NOME: ");
     scanf("%30[^\n]s", nome3);
-   while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    // fgets(nome3, 30, stdin);
+    clearBuffer();
     printf("SEXO [M/F]: ");
     sexo3 = getchar();
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    clearBuffer();
     printf("NOTA: ");
     scanf("%f", &nota3);
-    while ((ch = getchar()) != '\n' && ch != EOF);
-  if (fgets(buf, sizeof (buf), stdin))
-  {
-  }
+    clearBuffer();
     printf("LISTAGEM COMPLETA");
     printf("------------------------------\n");
     printf("NOME\t\tSEXO\tNOTA\n");
     printf("%s\t\t%c\t%.2f\n", nome1, sexo1, nota1);
     printf("%s\t\t%c\t%.2f\n", nome2, sexo2, nota2);
     printf("%s\t\t%c\t%.2f\n", nome3, sexo3, nota3);
-    return 0 ;
+}
+
+
+void clearBuffer(void){
+    char c;
+    while ((c = getchar())!= '\n' && c != EOF);
 }
