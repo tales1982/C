6131f8084789ba42f07475a323ad5d0f813a4b47
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-19 22:45:34 +0100

    resultado de expressão Par ou Impar

--- EXERCICIOS/Estudonauta/012/index.c
+++ EXERCICIOS/Estudonauta/012/index.c
@@ -6,7 +6,12 @@ int main(){
 printf("Digite um numero qualquer: \n");
 scanf("%i",&num1);
 setbuf(stdin,NULL);
-printf("O numero que vove digitou é %s\n",(num1%2==0)?"Par":"Impar");
+//printf("O numero que vove digitou é %s\n",(num1%2==0)?"Par":"Impar");
+if (num1%2 == 0){
+    printf("O numero que vove digitou é Par\n");
+}else{
+    printf("O numero que vove digitou é Impar\n");
+}
 
     return 0;
     }
