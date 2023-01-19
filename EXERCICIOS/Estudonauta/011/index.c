393b682d38447709df933e639e08fa5a314c9f33
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-19 20:25:42 +0100

    teste no mac

--- EXERCICIOS/Estudonauta/011/index.c
+++ EXERCICIOS/Estudonauta/011/index.c
@@ -1,62 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
-#define MAX 50
 
-int main()
-{
-    
-    char nome1[MAX];
-    char nome2[MAX];
-    char nome3[MAX];
-    char sexo1, sexo2, sexo3;
-    float nota1, nota2, nota3;
-    setlocale(LC_ALL, "Portuguese");
 
-    printf("Cadastrando a primeira pessoa:\n");
-    printf("------------------------------\n");
-    fflush(stdin);
-    printf("NOME: ");
-    fgets(nome1, MAX, stdin);
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    char product[30];
+    float price, finalPrice;
+
+    printf("Qual produto voce gostaria de compra.\n");
+    scanf("%29[^\n]s",product);
+    clearBuffer();
+    printf("Digite o preço do produto\n");
+    scanf("%f",&price);
+    clearBuffer();
     
-    fflush(stdin);
-    printf("SEXO [M/F] : ");
-    sexo1 = getchar();
-    scanf(" %c",&sexo1);
-    fflush(stdin);
-    printf("NOTA: ");
-    scanf("%f", &nota1);
-    fflush(stdin);
-    printf("Cadastrando a segunda pessoa:\n");
-    printf("------------------------------\n");
-    fflush(stdin);
-    printf("NOME: ");
-    
-    fgets(nome2, MAX, stdin);
-    
-    fflush(stdin);
-    printf("SEXO [M/F]: ");
-    scanf(" %c",&sexo2);
-    fflush(stdin);
-    printf("NOTA: ");
-    scanf("%f", &nota2);
-    fflush(stdin);
-    printf("Cadastrando a terceira pessoa:\n");
-    printf("------------------------------\n");
-    fflush(stdin);
-    printf("NOME: ");
-    fgets(nome3, MAX, stdin);
-    
-    fflush(stdin);
-    printf("SEXO [M/F]: ");
-    scanf(" %c",&sexo3);
-    fflush(stdin);
-    printf("NOTA: ");
-    scanf("%f", &nota3);
-    fflush(stdin);
-    printf("LISTAGEM COMPLETA");
-    printf("------------------------------\n");
-    printf("NOME\t\tSEXO\tNOTA\n");
-    printf("%s\t\t%c\t%.2f\n", nome1, sexo1, nota1);
-    printf("%s\t\t%c\t%.2f\n", nome2, sexo2, nota2);
-    printf("%s\t\t%c\t%.2f\n", nome3, sexo3, nota3);
+    float desc = 5 / 100 + price;
+    printf("%2f\n",desc);
+}
+
+void clearBuffer(void){
+    char c;
+    while ((c = getchar())!= '\n' && c != EOF);
 }
