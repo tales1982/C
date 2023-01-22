c2def58d365e58623e282b019a2b07ed3e3c02c9
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-22 20:13:25 +0100

    Calculando o percentual de aumento.

--- EXERCICIOS/Prof-Pietro/006-Salario-nenos-inposto/index.c
+++ EXERCICIOS/Prof-Pietro/006-Salario-nenos-inposto/index.c
@@ -17,13 +17,13 @@ int main(){
     fflush(stdin);
 
     float abatimento = (IMPOSTO/100)*salario;
-    printf("Voce teve %.2f Reais de desconto no salario\n",abatimento);
+    printf("Você teve %.2f Reais de desconto no salario\n",abatimento);
     float salarioAbatido = salario - abatimento;
     printf("Seu salario depois dos desconto é %.2f Reais\n",salarioAbatido);
-    float bonificacao = (BONUS/100)*salarioAbatido;
-    float salarioFinal = bonificacao + salarioAbatido;
-    printf("Esse meis voce teve uma bonificaçao de %.0f%% que e de %.2f Reais\n",BONUS,bonificacao);
-    printf("Seu salaro final depois dos desconto e o ajuste do bonus é de %.2f\n",salarioFinal);
+    float bonificação = (BONUS/100)*salarioAbatido;
+    float salarioFinal = bonificação + salarioAbatido;
+    printf("Esse mes você teve uma bonificação de %.0f%% que e de %.2f Reais\n",BONUS,bonificação);
+    printf("Seu salario final depois dos desconto e o ajuste do bonus é de %.2f\n",salarioFinal);
      //float salarioFinal = salario - abatimento;
     //printf("Seu salario ja descontado os imposto é de :%.2f\n",salarioFinal);
 
