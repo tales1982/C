4744274cf52732bfb20edfdb1df90d75448d721c
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-22 20:11:38 +0100

    Calculando o percentual de aumento.

--- EXERCICIOS/Prof-Pietro/005-salario-mais-almento/indec.c
+++ EXERCICIOS/Prof-Pietro/005-salario-mais-almento/indec.c
@@ -1,15 +1,18 @@
+/**
+ * Faça um programa que receba o salario de um funcionário e o percentual de aumento, calcule e mostre o valor do aumento e o novo salário.
+*/
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
-    float salario, almento;
+    float salario, aumento;
     puts("Digite seu salario base:");//o puts ja vem com a quebra de linha.
     scanf("%f",&salario);
     setbuf(stdin,NULL);
-    puts("Digite quantos \% voçe quer de almento.");
-    scanf("%f",&almento);
+    puts("Digite quantos \% você quer de aumento.");
+    scanf("%f",&aumento);
     setbuf(stdin,NULL);
-    float novoSalario = (almento / 100)*salario;
+    float novoSalario = (aumento / 100)*salario;
     novoSalario = novoSalario + salario;
     printf("O seu novo salario é de %.3f\n",novoSalario);
     return 0;
