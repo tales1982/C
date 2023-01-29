c7370196d4b738be23f7d306ffb1cd3dec250125
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-29 17:06:04 +0100

    update

---
+++ Ecole-WRC/Exercicios/003/index.c
@@ -0,0 +1,11 @@
+/*
+Desenvolva um programa em C que adicione o bônus de 10% ao salário de um
+vendedor, caso suas vendas ultrapassem R$ 10000,00 no mês. Se as vendas
+ultrapassarem R$ 20000,00, adicione 15% ao salário. Se o vendedor vender
+para R$ 30000,00 ou mais, adicione 25% ao salário.
+Exemplo: Salário R$ 4000,00
+Vendas igual ou abaixo de 10 MIL: Salário de R$ 4000,00
+Vendas acima de 10 MIL: Salário R$ 4400,00
+Vendas acima de 20 MIL: Salário R$ 4600,00
+Vendas igual ou acima de 30MIL: Salário de 5000,00
+*/
