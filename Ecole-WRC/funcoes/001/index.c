cc1df30efc982973ce5f3a4e688ac62a1d1c4224
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-29 17:02:42 +0100

    Exercicio 001

--- Ecole-WRC/funcoes/001/index.c
+++ Ecole-WRC/funcoes/001/index.c
@@ -1,3 +1,8 @@
+/*Escreva um programa em C que solicite o valor inteiro de dois operandos ao
+usuário e então imprima o resultado (verdadeiro ou falso) para os 6
+operadores relacionais existentes, de acordo com as entradas do usuário.
+Analise os resultados a cada execução do programa e veja se fazem sentido.
+*/
 #include <stdio.h>
 int main(){
     printf("teste");
