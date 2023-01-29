39a98ce6cf397cf46aa7cd2a522b0833d8f51dad
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-29 17:04:32 +0100

    update

---
+++ Ecole-WRC/Exercicios/002/index.c
@@ -0,0 +1,7 @@
+/*
+Implemente um programa em C que leia dois números inteiros e os apresente
+na tela em ordem decrescente.
+Exemplo: o usuário entra com 11 e 15. O sistema imprime 15 11.
+Exemplo: o usuário entra com 12 e 6. o sistema imprime 12 6.
+Exemplo: o usuário entre com 14 e 14. O sistema imprime 14 14.
+*/
