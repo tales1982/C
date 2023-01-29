0e8477ba9ab170b2d67f28efe14144bc01a2daf7
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-29 19:59:00 +0100

    Operadores relacionais + ternario

--- EXERCICIOS/Ecole-WRC/001-Operadores-Relacionais/index.c
+++ EXERCICIOS/Ecole-WRC/001-Operadores-Relacionais/index.c
@@ -5,5 +5,21 @@ Analise os resultados a cada execução do programa e veja se fazem sentido.
 */
 #include <stdio.h>
 int main(){
-    printf("teste");
-}
+	int n1 = 0, n2 = 0;
+
+   	 printf("Ola Digite 2 numeros para podermos comparar.\n");
+   	 printf("Digite 1° numero.\n");
+   	 scanf("%d",&n1);
+	 setbuf(stdin,NULL);
+	 printf("Digite o 2° numero\n");
+	 scanf("%d",&n2);
+	 setbuf(stdin,NULL);
+	 printf("========= RESULTADO  ==========\n");
+        printf("Comparando Igualdade: %d igual a  %d: %s.\n",n1,n2,(n1 == n2)?"Verdadeiro":"Falso");
+	printf("Comparando Diferente: %d é diferente de %d: %s\n",n1,n2,(n1 != n2)?"Verdadeiro":"Falso");
+	printf("Comparando Menor: %d é Menor  que %d: %s\n",n1, n2,(n1 < n2)?"Verdadeiro":"Falso");
+	printf("Comparando Maior: %d é Maior que %d: %s\n",n1, n2, (n1 > n2)?"Verdadeiro":"Falso");
+	printf("Comparando Menor ou Igual: %d é Menor ou Igual a: %d: %s\n",n1, n2, (n1 <= n2)?"Verdadeiro":"Falso");
+	printf("Comparando Maior ou Igual: %d é Maior ou Igual a: %d: %s\n",n1, n2, (n1 >= n2)?"Verdadeiro":"Falso");
+		return 0;
+ }
