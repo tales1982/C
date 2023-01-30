2dd566e3aa627a3f34bd344ecddc9b6613ff23db
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-01-30 22:43:04 +0100

    Em andamento

---
+++ EXERCICIOS/Prof-Pietro/SUB-ROTINAS/005-valor-de-quilowatt/index.c
@@ -0,0 +1,18 @@
+/*
+ * Sabe-se que o quillowatt de energia custa um milésimo do salario minimo: Faça um procedimento que receba o valor do salário
+ * minimo e quantidade de quillowatts consumida por uma residencia.O procedimento deve calcular e retornar atraveis de passagem de parametros por referencia:
+ * A) O valor, em reais, de cada quillowatt;
+ * B) O valor, em reais, a ser pago por essa residencia;
+ * C) O valor, em reais, a ser pago com desconto de 15%. 
+ * Sabendo disso, desenvolva um algoritimo que peça para inserir o valor do salário minimo e a quantidade de quillowatts consumida. Invoque o respectivo procedimento e mostre,
+ * na tela , as informacoes dos itens A , B , C. 
+ */
+
+/*================================================================================================================================
+                                                  Function Prototype   
+
+
+==================================================================================================================================
+*/
+// 						  Function Principal
+
