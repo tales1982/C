11f169cfbb4bf2641384ab0c0610325e3e944d1c
tales1982 <tales_lima_1982@hotmail.com>
2023-02-19 22:29:40 +0100

    Macros

---
+++ WR_Linguagem_C/macros/index.c
@@ -0,0 +1,24 @@
+#define multiplicacao(a,b) a * b
+#define divisao(a,b) a / b
+#define subtracao(a,b) a - b
+#define soma(a,b,c,d) a + b +c +d
+
+
+main()
+{
+int entrada1, entrada2, entrada3, entrada4;
+
+entrada1 = multiplicacao(17,5);
+printf("resultado da multiplicasao = %d\n",entrada1);
+
+entrada2 = divisao(58,6);
+printf("resultado da divisao = %d\n",entrada2);
+
+entrada3 = subtracao(28,6);
+printf("resultado da subtracao = %d\n",entrada3);
+
+entrada4 = soma(28,6,45,27);
+printf("resultado da soma = %d\n",entrada4);
+
+} 
+/* end main */
