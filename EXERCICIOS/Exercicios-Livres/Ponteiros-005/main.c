719f5b99babb89affbb9ae6afb7944d374a677b0
tales1982 <tales_lima_1982@hotmail.com>
2023-02-23 20:36:56 +0100

    exercicio 005

--- EXERCICIOS/Exercicios-Livres/Ponteiros-005/main.c
+++ EXERCICIOS/Exercicios-Livres/Ponteiros-005/main.c
@@ -2,7 +2,7 @@
 Faça um programa que leia dois valores inteiros e chame uma função que receba estes
 2 valores de entrada e retorne o maior valor na primeira variável e o menor valor na
 segunda variável. Escreva o conteúdo das 2 variáveis na tela.
-*/
+**/
 
 /* ======= Bibliotrca ========*/
 #include <stdio.h>
