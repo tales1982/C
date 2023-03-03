b065ea65c057e38357b45a20779d4e2044f0faf8
tales1982 <tales_lima_1982@hotmail.com>
2023-03-03 22:12:13 +0100

    Exercicio 002

---
+++ Prof-Pietro/ALGORITMO/002/main.c
@@ -0,0 +1,3 @@
+/*
+Desenvolva um algoritmo que receba três números. O algoritmo deve imprimir "Condição satisfeita", na tela , caso o primeiro dado inserido seja maior do que os outros dois (o primeiro nao pode ser igual a nenhum). Caso contrario, devo ser impressa a mensagem "ERRO".
+*/
