cd063d78722c92ce5523d89b57e6ca0168f286d2
tales1982 <tales_lima_1982@hotmail.com>
2023-03-05 22:06:46 +0100

    Calculando Media

--- Prof_Antonio_H_Reis/01-EXERCICIOS/002/main.c
+++ Prof_Antonio_H_Reis/01-EXERCICIOS/002/main.c
@@ -1,7 +1,24 @@
+/*
+Escreva um programa que leia tres notas de uma aluno e calcule sua media*/
 #include<stdio.h>
 
 int main()
 {
-printf("MEU\n\n\tSEGUNDO\n\n\t\tPROGRAMA\n\n\t\t\tEM LIGUAGEM C");
+    float a, b, c, res;
 
+printf("Digite sua 1° nota.\n");
+scanf("%f",&a);
+setbuf(stdin,NULL);
+
+printf("Digite sua 2° nota.\n");
+scanf("%f",&b);
+setbuf(stdin,NULL);
+
+printf("Digite sua 3° nota.\n");
+scanf("%f",&c);
+setbuf(stdin,NULL);
+
+res = (a+b+c)/3;
+
+printf("Sua media final foi de %.1f\n",res);
 }
