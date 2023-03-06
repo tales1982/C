56620ff38ecbad6de7f3090329b8a8dfaa4235a6
tales1982 <tales_lima_1982@hotmail.com>
2023-03-06 22:36:33 +0100

    Numero secreto

--- Prof_Antonio_H_Reis/01-EXERCICIOS/005/main.c
+++ Prof_Antonio_H_Reis/01-EXERCICIOS/005/main.c
@@ -1,6 +1,6 @@
 /*
  * Escreve um programa que leia um numero inteiro qualquer.
- * Informar se o numero e diferente de 1000 ou se é igual.
+ * Informar se o numero e diferente de 1000 ou se é igual..
  */
 
 #include<stdio.h>
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
   int a = 10, b;
-  printf("Digite um numero, para ver se voce acerta o numero secreto!\n");
+  printf("Digite um numero, para ver se voce acerta o numero secreto..!\n");
   scanf("%i",&b);
 
   if(b == a)printf("BRAVO voce acertou o numero secreto.\n");else printf("Erro seu número e diferente do número secreto\n");
