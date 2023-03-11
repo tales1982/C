bb002a1b90d66c405d7955ace59b6eaa94e62a1f
tales1982 <tales_lima_1982@hotmail.com>
2023-03-11 09:24:51 +0100

    Imprimindo letras na posicao impar.

--- Coletanea_d_exercicios/002-imprimir_letras_impar/main.c
+++ Coletanea_d_exercicios/002-imprimir_letras_impar/main.c
@@ -25,24 +25,3 @@ char nome[30];
   return 0;
 }
 
-// outra maeira de fazer usando a biblioteca string.h
-/*
-int main(int argc, char *argv[])
-{
-   int i,tam,x;
-   char nome[30];
-printf("Informe um nome.");
-fgets(nome,30,stdin);
-
-tam = strlen(nome);//strlen informa o tamanho da da variavel exe= (tales tamanho 5)
-
-for (i = 0; i <= tam -1 ;i = i+2) {
- //o tam - 1 e para eliminar o espaco no final da impresao do codigo.
-  printf("%c\n",nome[i]);
-}
-
-
-  return 0;
-}
-
-*/
