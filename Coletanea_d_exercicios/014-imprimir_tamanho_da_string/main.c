a58a3b555fc8390b1721398748431da21e81299a
tales1982 <tales_lima_1982@hotmail.com>
2023-03-14 20:07:32 +0100

    update

---
+++ Coletanea_d_exercicios/014-imprimir_tamanho_da_string/main.c
@@ -0,0 +1,29 @@
+/* Informe dois nomes, imprimir o tamanho de cada um e mostra-los */
+
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+  char nome1[15],nome2[15];
+  int tam1 = 0,tam2 = 0;
+
+
+  printf("Digite o 1° e 2° nome");
+  scanf("%s %s",nome1,nome2);
+
+  if(strcmp(nome1,nome2) == 0){
+
+    printf("iguais \n");
+  }
+  
+  printf("%i",strcmp(nome1,nome2));
+ // tam1 = strlen(nome1);
+ // tam2 = strlen(nome2);
+
+//  printf("Tamanho 1 = %i\n",tam1);
+ // printf("Tamanho 2 = %i\n",tam2);
+
+
+  return 0;
+}
