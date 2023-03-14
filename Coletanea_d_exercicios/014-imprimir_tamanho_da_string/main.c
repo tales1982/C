be27c741f79c154673c67457db0f93a5327264ab
tales1982 <tales_lima_1982@hotmail.com>
2023-03-14 21:27:29 +0100

    Imprimindo tamanho da strig

--- Coletanea_d_exercicios/014-imprimir_tamanho_da_string/main.c
+++ Coletanea_d_exercicios/014-imprimir_tamanho_da_string/main.c
@@ -9,21 +9,19 @@ int main(int argc, char *argv[])
   int tam1 = 0,tam2 = 0;
 
 
-  printf("Digite o 1° e 2° nome");
-  scanf("%s %s",nome1,nome2);
+  printf("Digite o 1° nome\n");
+  scanf("%s",nome1);
+  setbuf(stdin,NULL);
 
-  if(strcmp(nome1,nome2) == 0){
+  printf("Digite o 2° nome");
+  scanf("%s",nome2);
 
-    printf("iguais \n");
-  }
   
-  printf("%i",strcmp(nome1,nome2));
- // tam1 = strlen(nome1);
- // tam2 = strlen(nome2);
+  tam1 = strlen(nome1);
+  tam2 = strlen(nome2);
 
-//  printf("Tamanho 1 = %i\n",tam1);
- // printf("Tamanho 2 = %i\n",tam2);
-/*terstedsakljdsefjgklöglösdfk,*/
-/*sdfsfdsdfdsf*/
+  printf("Tamanho do 1° nome = %i\n",tam1);
+  printf("Tamanho do 2° nome = %i\n",tam2);
+
   return 0;
 }
