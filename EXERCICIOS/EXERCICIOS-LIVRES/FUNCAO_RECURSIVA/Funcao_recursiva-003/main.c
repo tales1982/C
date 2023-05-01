ff1e66346de0a878f0d06805863d1145911a3fc9
tales1982 <tales_lima_1982@hotmail.com>
2023-05-01 13:44:11 +0200

    C

---
+++ EXERCICIOS/EXERCICIOS-LIVRES/FUNCAO_RECURSIVA/Funcao_recursiva-003/main.c
@@ -0,0 +1,33 @@
+/*
+ Faça uma fuçao recursivaue calcule e retorne o enésimo termo da sequêcia fibonacci.
+ Alguns números desta sequência sao 0,1,1,2,3,5,8,13,21,34,55,89......
+ Fórmula de Fibonacci
+
+=========================================================================================
+A sucessão de Fibonacci é uma sequência de números inteiros iniciados por zero e um,
+no qual cada termo subsequente corresponde a soma dos dois números anteriores:
+0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584...  
+*/
+
+#include<stdio.h>
+
+int fibonacci(int n);
+
+int main(int argc, char *argv[])
+{
+  int n;
+  printf("Digite um valor para calcula o enésimo termo de fibonacci..: ");
+  scanf("%i",&n);
+  setbuf(stdin,NULL);
+  printf("Resultado..: %i\n",fibonacci(n));
+  return 0;
+}
+
+int fibonacci(int n)
+{
+  if(n < 2){
+    return n;
+  }else{
+    return fibonacci(n -1) + fibonacci(n - 2);
+  }
+}
