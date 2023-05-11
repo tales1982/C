a2a43558117095bd4518fdbe211656894f9f70ec
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-05-11 13:46:39 +0200

    parc mat loc

--- EXERCICIOS/Exercicios_WR/Basico/007-switch/index.c
+++ EXERCICIOS/Exercicios_WR/Basico/007-switch/index.c
@@ -12,3 +12,135 @@ Após digitar idade e estado civil, por exemplo 34 para idade e C para o estado
 civil, a saída do programa deverá ser:
 “O indivíduo tem 34 anos e é casado”.
 */
+
+/*==================================loca parca mat (MAIN)====================================================*/
+#include<stdio.h>
+#include<string.h>
+#include"menu.h"
+
+
+/*------------------------ MAIN ----------------
+int main() {
+int opcao;
+
+do{
+printf(" _____________________________________________ \n");
+printf("|                                             |\n");
+printf("| 1 - Consulte machines jusq a 3 Tonnes.      |\n");
+printf("| 2 - Consulte machines de 4 a 6 tonnes.      |\n");
+printf("| 3 - Consulte machines de 7 a 10 tonnes.     |\n");
+printf("| 4 - Consulte petite material.               |\n");
+printf("| 5 - SORTI..                                 |\n");
+printf("|_____________________________________________|\n");
+
+scanf("%i",&opcao);
+setbuf(stdin,NULL);
+
+if(opcao == 1){
+
+}else if(opcao == 2){
+menuVio50();
+}else if(opcao == 3){
+
+
+}else if(opcao == 4) { 
+
+
+}else if (opcao == 5) {
+printf("SORTI.....\n");
+}else{
+printf("ERRO...\n");
+}
+
+
+
+}while(opcao != 5);
+
+return 0;
+}
+*/
+
+
+/* =================================================== menu.h ===================================
+
+
+typedef struct{
+int serie,
+int hrs,
+char client[30],
+}Machines;
+
+
+void copiandoArquivos(FILE *file1, FILE *file2);
+
+void menuVio50();
+*/
+ 
+/*============================== menuVio50 ==================================
+#include<stdio.h>
+#include<string.h>
+#include"menu.h"
+
+
+void menuVio50()
+{
+int id = 12; 
+
+// Machine[id] vio50;
+/*
+for(int i = 0; i < id; i++){i
+}
+*/
+
+/*
+printf(" _____________________________________________________________________________________\n");
+printf("|                                  PARC MATERIAL                                      |\n");
+printf("|                                     VIO 50                                          |\n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c | CLIENTE..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER...: %c | CLIENT..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c | CLIENTE..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER...: %c | CLIENT..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+printf("|                                                                                     |\n");
+printf("| VIO50 SERIE..: 0000 | HRS %i | LOYER..: %c  CLIENTE ..: %s                               \n");
+printf("|_____________________________________________________________________________________|\n");
+
+}
+
+
+*/
+  
+  
+  
+  
+  
+  
