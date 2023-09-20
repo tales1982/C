bdb77bf116b011c4f7cbfb0b7bce93919ceed8d2
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-09-20 12:02:25 +0200

    Exercicio 2

---
+++ LUIZ-DAMAS/capitulo-01/002/exer2.c
@@ -0,0 +1,15 @@
+// Escreva um programa que coloque na tela a seguinte arvore
+//               *
+//              ***
+//             *****               
+//                   
+
+#include <stdio.h>
+
+main(){
+
+    printf("   *\n  ***\n *****\n  /|\\");
+
+}
+
+
