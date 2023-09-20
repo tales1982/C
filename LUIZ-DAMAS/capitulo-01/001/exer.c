dbf25395c2d0028fc0cee4281a6d9a7c7f56da95
Tales Lima <102876696+tales1982@users.noreply.github.com>
2023-09-20 11:03:47 +0200

    Capitulo 01 Exercico 01

---
+++ LUIZ-DAMAS/capitulo-01/001/exer.c
@@ -0,0 +1,12 @@
+//Ecreva na tela um programa que escreva a segionte frase: 
+//Bem vindo ao /Mundo\ da programacao em "c"
+
+#include <stdio.h>
+
+main(){
+
+
+    printf("Bem vindo ao /Mundo\\ da programacao em \"c\"");
+
+}
+
