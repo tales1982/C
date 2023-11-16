38561941936d6b9c548949bbecd67f0af7f798eb
Tales Lima <tales_lima_1982@hotmail.com>
2023-11-16 22:11:01 +0100

    exercicio 2

---
+++ EXERCICIOS/THALES-CEZARE/exer-02/main.c
@@ -0,0 +1,18 @@
+/*
+ escreva um algoritimo que leia 3 notas e calcule e imprima a media aritimetica.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	float n1=8,n2=7,n3=9;
+
+	float res = (n1+n2+n3)/3;
+
+	printf("A media e %.2f",res);
+
+
+}
+
