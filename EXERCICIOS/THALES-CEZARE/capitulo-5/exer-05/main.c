39a46ce27894aa8ba0d8e6df0fa4a9d84c4dc688
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-01-04 22:45:34 +0100

    capitulo-5

--- EXERCICIOS/THALES-CEZARE/capitulo-5/exer-05/main.c
+++ EXERCICIOS/THALES-CEZARE/capitulo-5/exer-05/main.c
@@ -12,7 +12,7 @@
  a maior idade dos habitantes
  a porcentagem de pessoas do sexo feminino cuja idade esta entre 18 e 25 anos
  a porcentagem de pessoas com olhos claros
- Obs: O ultimo dao contém uma idade negativa.
+ Obs: O ultimo dado contém uma idade negativa.
  */
 
 #include <stdio.h>
@@ -34,17 +34,20 @@ void limparBuffer() {
 
 int	main()
 {
-	Habitantes pessoa[TAM];
-	float media_todos;
-	float media_h;
-	float media_f;
+	Habitantes	pessoa[TAM];
+	float	media_todos;
+	float	media_h;
+	float	media_f;
 	int	count;
-	int count_h;
-	int count_f;
-	float count_j;
-	float porceto_j;
+	int	count_h;
+	int	count_f;
+	float	porceto_j;
 	int	opcao;
-	int aux_maior_idade;
+	int	aux_maior_idade;
+	int	countMulheresEntre18e25;
+	float	porcentagem;
+	float	porcentagem_olhos;
+	int	olhosClaros;
 
 	opcao = 1;
 	media_todos = 0.0;
@@ -53,9 +56,12 @@ int	main()
 	count = 0;
 	count_h = 0;
 	count_f = 0;
-	count_j = 0.0;
 	porceto_j = 0.0;
 	aux_maior_idade = 0;
+	countMulheresEntre18e25 = 0;
+	porcentagem = 0.0;
+	porcentagem_olhos = 0.0;
+	olhosClaros = 0;
 	
 	puts("\t __________________________");
 	puts("\t|                          |");
@@ -91,15 +97,20 @@ int	main()
 		{
 			media_f += pessoa[count].idade;
 			count_f++;
+			if(pessoa[count].idade >= 18 && pessoa[count].idade <= 25)
+			{
+				countMulheresEntre18e25++;
+			}
 		}
 		if(pessoa[count].idade > aux_maior_idade)
 		{
 			aux_maior_idade = pessoa[count].idade;
 		}
-		if(pessoa[count].idade >= 18 && pessoa[count].idade <= 25)
+		if(pessoa[count].olhos == 'a' || pessoa[count].olhos == 'A' && pessoa[count].olhos == 'v' || pessoa[count].olhos == 'V')
 		{
-			count_j++;
+			olhosClaros++;
 		}
+		
 		count++;
 
 
@@ -108,12 +119,17 @@ int	main()
 	media_todos /= count;
 	media_h /= count_h;
 	media_f /= count_f;
+	porcentagem = ((float)countMulheresEntre18e25 / count) * 100;
+	porcentagem_olhos = ((float)olhosClaros / count) * 100;
+	
+	
+	printf("\n\tMedia de idade de todos os habitantes... %.2f\n",media_todos);
+	printf("\tMedia de idade dos hommes... %.2f\n",media_h);
+	printf("\tMedia de idade das mulheres... %.2f\n",media_f);
+	printf("\tA maior idade e de... %d anos\n",aux_maior_idade);
+	printf("\tPorcentagem de mulheres entre 18 e 25 anos: %.2f%%\n", porcentagem);
+	printf("\tPorcentagem de pessoas com os olhos claros : %.2f%%\n", porcentagem_olhos);
 
-	printf("DEBUG[%.1f]",count_j);
-	printf("Media de idade de todos os habitantes... %.2f\n",media_todos);
-	printf("Media de idade dos hommes... %.2f\n",media_h);
-	printf("Media de idade das mulheres... %.2f\n",media_f);
-	printf("A maior idade e de... %d anos\n",aux_maior_idade);
 
 
 	return(0);
