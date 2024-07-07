d0399f1585e2831ef4c53873a2b59d34ab4fa77b
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-07-07 10:21:26 +0200

    delete file

--- EXERCICIOS/Exercicios_WR/Basico/006-Operador-condicional/index.c
+++
@@ -1,8 +0,0 @@
-/*
-Escreva um programa em C para emitir um alerta via console quando a tensão
-de uma determinada bateria estiver abaixo de 10 Volts. Caso contrário, o
-sistema informa que a tensão está OK.
-O usuário entra com o valor da tensão em número real via teclado e o sistema
-realiza a verificação e informa o status.
-Utilize o operador interrogação ( ? ) para o teste condicional.
-*/
